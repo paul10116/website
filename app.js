94b838de5b998876973da4fb45a084316379355d
paul10116 <paulius.beinoras@gmail.com>
2022-11-03 09:25:03 +0200

    2022.11.03

---
+++ app.js
@@ -0,0 +1,29 @@
+let shop = document.getElementById("#shop");
+
+fetch("products.json")
+  .then((res) => res.json())
+  .then((products) => {
+    products.map((product) => {
+      let productElement = document.createElement("div");
+
+      productElement.innerHTML = `
+      
+        <div class="product-card">
+          <img src="${product.image}"/>
+          <div class="description">
+            <h4>${product.model}</h4>
+            <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p> In store: ${product.in_store}</p>
+          </div>
+        </div>`;
+      console.log(productElement);
+
+      shop.append(productElement);
+    });
+  });
